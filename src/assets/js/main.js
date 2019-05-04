12f41f486112a9074a9daf134e39f289f3a5dee5
Nikita <nikitossuv@gmail.com>
2019-05-04 19:25:53 +0300

    UI

---
+++ src/assets/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Section: jQuery
+ */
+
+$(function() {
+	
+	//@@include('general/global/global.js')
+	//@@include('general/ui/ui.js')
+	//@@include('general/components/components.js')
+	//@@include('general/sections/sections.js')
+	
+});
